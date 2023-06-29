ccf749bf5f7a4f28482abd2bba142c24defd9e63
Léa <1997.belier@gmail.com>
2023-06-29 08:58:47 -0400

    1 step training and data augmentation

---
+++ LUSR/main.py
@@ -0,0 +1,446 @@
+import argparse
+
+import numpy as np
+
+import gym
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Beta
+from torch.utils.data.sampler import BatchSampler, SubsetRandomSampler
+from utils import Resutls
+import cv2
+from torchvision.utils import save_image
+from utils import RandomTransform
+
+parser = argparse.ArgumentParser(description='Train a PPO agent for the CarRacing-v0')
+parser.add_argument('--gamma', type=float, default=0.99, metavar='G', help='discount factor (default: 0.99)')
+parser.add_argument('--encoder-path', default='/home/mila/l/lea.cote-turcotte/LUSR/checkpoints/encoder.pt')
+parser.add_argument('--train-encoder', default=False, type=bool)
+parser.add_argument('--learning-rate', default=0.0001, type=float)
+parser.add_argument('--latent-size', default=16, type=int)
+parser.add_argument('--action-repeat', type=int, default=8, metavar='N', help='repeat action in N frames (default: 8)')
+parser.add_argument('--img-stack', type=int, default=3, metavar='N', help='stack N image in a state (default: 4)')
+parser.add_argument('--seed', type=int, default=0, metavar='N', help='random seed (default: 0)')
+parser.add_argument('--render', action='store_true', help='render the environment')
+parser.add_argument('--vis', action='store_true', help='use visdom')
+parser.add_argument('--beta', default=10, type=int)
+parser.add_argument('--bloss-coef', default=1, type=int)
+parser.add_argument('--class-latent-size', default=8, type=int)
+parser.add_argument('--content-latent-size', default=32, type=int)
+parser.add_argument(
+    '--log-interval', type=int, default=10, metavar='N', help='interval between training status logs (default: 10)')
+args = parser.parse_args()
+
+use_cuda = torch.cuda.is_available()
+#device = torch.device("cuda" if use_cuda else "cpu")
+device = torch.device("cpu")
+torch.manual_seed(args.seed)
+if use_cuda:
+    torch.cuda.manual_seed(args.seed)
+
+transition = np.dtype([('s', np.float64, (3, 64, 64)), ('a', np.float64, (3,)), ('a_logp', np.float64),
+                       ('r', np.float64), ('s_', np.float64, (3, 64, 64))])
+
+class Env():
+    """
+    Environment wrapper for CarRacing 
+    """
+
+    def __init__(self):
+        self.env = gym.make('CarRacing-v0')
+        self.env.seed(args.seed)
+        self.reward_threshold = self.env.spec.reward_threshold
+
+    def process_obs(self, obs): # a single frame (96, 96, 3) for CarRacing
+        obs = cv2.resize(obs[:84, :, :], dsize=(64,64), interpolation=cv2.INTER_NEAREST)
+        #return np.transpose(obs, (2,0,1))
+        return obs
+
+    def reset(self):
+        self.counter = 0
+        self.av_r = self.reward_memory()
+
+        self.die = False
+        img_rgb = self.env.reset()
+        processed_obs = self.process_obs(img_rgb)
+        save_image(torch.tensor(processed_obs.copy(), dtype=torch.float32), "/home/mila/l/lea.cote-turcotte/LUSR/checkimages/env_ppo_carracing.png")
+        return processed_obs
+
+    def step(self, action):
+        total_reward = 0
+        for i in range(args.action_repeat):
+            img_rgb, reward, die, _ = self.env.step(action)
+            # don't penalize "die state"
+            if die:
+                reward += 100
+            # green penalty
+            if np.mean(img_rgb[:, :, 1]) > 185.0:
+                reward -= 0.05
+            total_reward += reward
+            # if no reward recently, end the episode
+            done = True if self.av_r(reward) <= -0.1 else False
+            if done or die:
+                break
+        img_rgb = self.process_obs(img_rgb)
+        return img_rgb, total_reward, done, die
+
+    def render(self, *arg):
+        self.env.render(*arg)
+
+    @staticmethod
+    def rgb2gray(rgb, norm=True):
+        # rgb image -> gray [0, 1]
+        gray = np.dot(rgb[..., :], [0.299, 0.587, 0.114])
+        if norm:
+            # normalize
+            gray = gray / 128. - 1.
+        return gray
+
+    @staticmethod
+    def reward_memory():
+        # record reward for last 100 steps
+        count = 0
+        length = 100
+        history = np.zeros(length)
+
+        def memory(reward):
+            nonlocal count
+            history[count] = reward
+            count = (count + 1) % length
+            return np.mean(history)
+
+        return memory
+
+class Encoder(nn.Module):
+    def __init__(self, class_latent_size = 8, content_latent_size = 32, input_channel = 3, flatten_size = 1024):
+        super(Encoder, self).__init__()
+        self.class_latent_size = class_latent_size
+        self.content_latent_size = content_latent_size
+        self.flatten_size = flatten_size
+
+        self.main = nn.Sequential(
+            nn.Conv2d(input_channel, 32, 4, stride=2), nn.ReLU(),
+            nn.Conv2d(32, 64, 4, stride=2), nn.ReLU(),
+            nn.Conv2d(64, 128, 4, stride=2), nn.ReLU(),
+            nn.Conv2d(128, 256, 4, stride=2), nn.ReLU()
+        )
+
+        self.linear_mu = nn.Linear(flatten_size, content_latent_size)
+        self.linear_logsigma = nn.Linear(flatten_size, content_latent_size)
+        self.linear_classcode = nn.Linear(flatten_size, class_latent_size) 
+
+    def forward(self, x):
+        x = self.main(x)
+        x = x.view(x.size(0), -1)
+        mu = self.linear_mu(x)
+        logsigma = self.linear_logsigma(x)
+        classcode = self.linear_classcode(x)
+
+        return mu, logsigma, classcode
+
+    def get_feature(self, x):
+        mu, logsigma, classcode = self.forward(x)
+        return mu
+
+
+class Decoder(nn.Module):
+    def __init__(self, latent_size = 32, output_channel = 3, flatten_size=1024):
+        super(Decoder, self).__init__()
+
+        self.fc = nn.Linear(latent_size, flatten_size)
+
+        self.main = nn.Sequential(
+            nn.ConvTranspose2d(flatten_size, 128, 5, stride=2), nn.ReLU(),
+            nn.ConvTranspose2d(128, 64, 5, stride=2), nn.ReLU(),
+            nn.ConvTranspose2d(64, 32, 6, stride=2), nn.ReLU(),
+            nn.ConvTranspose2d(32, 3, 6, stride=2), nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.fc(x)
+        x = x.unsqueeze(-1).unsqueeze(-1)
+        x = self.main(x)
+        return x
+
+class DisentangledVAE(nn.Module):
+    def __init__(self, class_latent_size = 8, content_latent_size = 32, img_channel = 3, flatten_size = 1024):
+        super(DisentangledVAE, self).__init__()
+        self.encoder = Encoder(class_latent_size, content_latent_size, img_channel, flatten_size)
+        self.decoder = Decoder(class_latent_size + content_latent_size, img_channel, flatten_size)
+
+    def forward(self, x):
+        mu, logsigma, classcode = self.encoder(x)
+        contentcode = reparameterize(mu, logsigma)
+        latentcode = torch.cat([contentcode, classcode], dim=1)
+
+        recon_x = self.decoder(latentcode)
+
+        return mu, logsigma, classcode, recon_x
+
+class ActorCriticNet(nn.Module):
+    def __init__(self, model_config, content_latent_size=32):
+        nn.Module.__init__(self)
+
+        self.main = Encoder()   
+        self.critic = nn.Sequential(nn.Linear(content_latent_size, 400), nn.ReLU(), nn.Linear(400, 300), nn.ReLU(), nn.Linear(300, 1))
+        self.actor = nn.Sequential(nn.Linear(content_latent_size, 400), nn.ReLU(), nn.Linear(400, 300), nn.ReLU())
+        self.alpha_head = nn.Sequential(nn.Linear(300, 3), nn.Softplus())
+        self.beta_head = nn.Sequential(nn.Linear(300, 3), nn.Softplus())
+        self.train_encoder = model_config['train_encoder']
+        self.apply(self._weights_init)
+        print("Train Encoder: ", self.train_encoder)
+
+    @staticmethod
+    def _weights_init(m):
+        if isinstance(m, nn.Conv2d):
+            nn.init.xavier_uniform_(m.weight, gain=nn.init.calculate_gain('relu'))
+            nn.init.constant_(m.bias, 0.1)
+
+    def forward(self, input):
+        features = self.main.get_feature(input)
+        if not self.train_encoder:
+            features = features.detach()  # not train the encoder
+
+        v = self.critic(features)
+        actor_features = self.actor(features)
+        alpha = self.alpha_head(actor_features) + 1
+        beta = self.beta_head(actor_features) + 1
+
+        return (alpha, beta), v
+
+class CycleVAE():
+    def __init__(self, model_config):
+        nn.Module.__init__(self)
+        self.model_config = model_config
+        self.model = DisentangledVAE(class_latent_size = self.model_config['class_latent_size'], content_latent_size = self.model_config['content_latent_size']).double().to(device)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.model_config['learning_rate'])
+
+    def vae_loss(self, x, mu, logsigma, recon_x, beta=1):
+        recon_loss = F.mse_loss(x, recon_x, reduction='mean')
+        kl_loss = -0.5 * torch.sum(1 + logsigma - mu.pow(2) - logsigma.exp())
+        kl_loss = kl_loss / torch.numel(x)
+        return recon_loss + kl_loss * beta
+
+    def reparameterize(self, mu, logsigma):
+        std = torch.exp(0.5*logsigma)
+        eps = torch.randn_like(std)
+        return mu + eps*std
+
+    def forward_loss(self, x, beta):
+        mu, logsigma, classcode = self.self.model.encoder(x)
+        contentcode = self.reparameterize(mu, logsigma)
+        shuffled_classcode = classcode[torch.randperm(classcode.shape[0])]
+
+        latentcode1 = torch.cat([contentcode, shuffled_classcode], dim=1)
+        latentcode2 = torch.cat([contentcode, classcode], dim=1)
+
+        recon_x1 = self.model.decoder(latentcode1)
+        recon_x2 = self.model.decoder(latentcode2)
+
+        return vae_loss(x, mu, logsigma, recon_x1, beta) + vae_loss(x, mu, logsigma, recon_x2, beta)
+
+
+    def backward_loss(x, device):
+        mu, logsigma, classcode = self.model.encoder(x)
+        shuffled_classcode = classcode[torch.randperm(classcode.shape[0])]
+        randcontent = torch.randn_like(mu).to(device)
+        print((randcontent.shape, classcode.shape))
+
+        latentcode1 = torch.cat([randcontent, classcode], dim=1)
+        latentcode2 = torch.cat([randcontent, shuffled_classcode], dim=1)
+        print(latentcode2.shape)
+
+        recon_imgs1 = self.model.decoder(latentcode1).detach()
+        recon_imgs2 = self.model.decoder(latentcode2).detach()
+
+        cycle_mu1, cycle_logsigma1, cycle_classcode1 = self.model.encoder(recon_imgs1)
+        cycle_mu2, cycle_logsigma2, cycle_classcode2 = self.model.encoder(recon_imgs2)
+
+        cycle_contentcode1 = self.reparameterize(cycle_mu1, cycle_logsigma1)
+        cycle_contentcode2 = self.reparameterize(cycle_mu2, cycle_logsigma2)
+
+        bloss = F.l1_loss(cycle_contentcode1, cycle_contentcode2)
+        return bloss
+    
+    def update(self, s, s_, index, training_step, results_vae):
+        imgs = RandomTransform(s[index]).apply_transformations()
+        imgs2 = s_[torch.randperm(index.shape[0])]
+        save_image(imgs, "/home/mila/l/lea.cote-turcotte/LUSR/checkimages/imgs.png")
+        save_image(imgs2, "/home/mila/l/lea.cote-turcotte/LUSR/checkimages/imgs2.png")
+
+        self.optimizer.zero_grad()
+
+        floss = 0
+        for i_class in range(imgs.shape[0]): # imgs.shape[0] = 5 
+             # batch size 10 for each class (5 class)
+            image = imgs[i_class]
+            floss += self.forward_loss(image, self.model_config['beta'])
+            save_image(image, "/home/mila/l/lea.cote-turcotte/LUSR/checkimages/%d.png" % (i_class))
+        floss = floss / imgs.shape[0] # divided by the number of classes
+
+        # backward circle
+        imgs = imgs.reshape(-1, *imgs.shape[2:]) # from torch.Size([5, 10, 3, 64, 64]) to torch.Size([50, 3, 64, 64])
+        # batch of 50 imgaes with mix classes
+        bloss = self.backward_loss(imgs, model, device)
+
+        loss = floss + bloss * self.model_config['bloss_coef']
+        results.update_logs(["training_step", "loss"], [training_step, loss])
+
+        (floss + bloss * self.model_config['bloss_coef']).backward()
+        print(loss)
+        optimizer.step()
+
+        # save image to check and save model 
+        if training_step % 10 == 0:
+            rand_idx = torch.randperm(imgs.shape[0])
+            imgs1 = imgs[rand_idx[:9]]
+            imgs2 = imgs2[rand_idx[-9:]]
+            with torch.no_grad():
+                mu, _, classcode1 = self.model.encoder(imgs1)
+                _, _, classcode2 = self.model.encoder(imgs2)
+                recon_imgs1 = self.model.decoder(torch.cat([mu, classcode1], dim=1))
+                recon_combined = self.model.decoder(torch.cat([mu, classcode2], dim=1))
+
+            saved_imgs = torch.cat([imgs1, imgs2, recon_imgs1, recon_combined], dim=0)
+            save_image(saved_imgs, "/home/mila/l/lea.cote-turcotte/LUSR/checkimages/%d_%d_%d.png" % (i_epoch, i_split,i_batch), nrow=9)
+
+
+class Agent():
+    """
+    Agent for training
+    """
+    max_grad_norm = 0.5
+    clip_param = 0.1  # epsilon in clipped loss
+    ppo_epoch = 10
+    buffer_capacity, batch_size = 2000, 128
+    batch_size_vae = 400
+
+    def __init__(self, model_config, results_vae):
+        self.training_step = 0
+        self.net = ActorCriticNet(model_config).double().to(device)
+        self.cycle_vae = CycleVAE(model_config)
+        self.buffer = np.empty(self.buffer_capacity, dtype=transition)
+        self.counter = 0
+
+        self.optimizer = optim.Adam(self.net.parameters(), lr=1e-3)
+
+    def select_action(self, state):
+        state = torch.from_numpy(state).double().to(device).unsqueeze(0)
+        with torch.no_grad():
+            alpha, beta = self.net(state)[0]
+        dist = Beta(alpha, beta)
+        action = dist.sample()
+        a_logp = dist.log_prob(action).sum(dim=1)
+
+        action = action.squeeze().cpu().numpy()
+        a_logp = a_logp.item()
+        return action, a_logp
+
+    def save_param(self):
+        torch.save(self.net.state_dict(), '/home/mila/l/lea.cote-turcotte/LUSR/param/ppo_net_params.pkl')
+
+    def store(self, transition):
+        self.buffer[self.counter] = transition
+        self.counter += 1
+        if self.counter == self.buffer_capacity:
+            self.counter = 0
+            return True
+        else:
+            return False
+
+    def update(self):
+        self.training_step += 1
+
+        s = torch.tensor(self.buffer['s'], dtype=torch.double).to(device)
+        a = torch.tensor(self.buffer['a'], dtype=torch.double).to(device)
+        r = torch.tensor(self.buffer['r'], dtype=torch.double).to(device).view(-1, 1)
+        s_ = torch.tensor(self.buffer['s_'], dtype=torch.double).to(device)
+
+        old_a_logp = torch.tensor(self.buffer['a_logp'], dtype=torch.double).to(device).view(-1, 1)
+
+        with torch.no_grad():
+            target_v = r + model_config['gamma'] * self.net(s_)[1]
+            adv = target_v - self.net(s)[1]
+            #adv = (adv - adv.mean()) / (adv.std() + 1e-8)
+
+        training_step = self.training_step
+        for _ in range(3):
+            training_step =+ 1
+            for index in BatchSampler(SubsetRandomSampler(range(self.buffer_capacity)), self.batch_size, False):
+                training_step =+ 1
+                self.cycle_vae.update(s, s_, index, training_step, results_vae)
+
+        for _ in range(self.ppo_epoch):
+            for index in BatchSampler(SubsetRandomSampler(range(self.buffer_capacity)), self.batch_size_vae, False):
+
+                alpha, beta = self.net(s[index])[0]
+                dist = Beta(alpha, beta)
+                a_logp = dist.log_prob(a[index]).sum(dim=1, keepdim=True)
+                ratio = torch.exp(a_logp - old_a_logp[index])
+
+                surr1 = ratio * adv[index]
+                surr2 = torch.clamp(ratio, 1.0 - self.clip_param, 1.0 + self.clip_param) * adv[index]
+                action_loss = -torch.min(surr1, surr2).mean()
+                value_loss = F.smooth_l1_loss(self.net(s[index])[1], target_v[index])
+                loss = action_loss + 2. * value_loss
+
+                self.optimizer.zero_grad()
+                loss.backward()
+                # nn.utils.clip_grad_norm_(self.net.parameters(), self.max_grad_norm)
+                self.optimizer.step()
+
+
+if __name__ == "__main__":
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    model_config = {'encoder_path':args.encoder_path, 'train_encoder':args.train_encoder, 'latent_size':args.latent_size, 'beta': args.beta, 'bloss_coef': args.bloss_coef, 'class_latent_size': args.class_latent_size, 'content_latent_size': args.content_latent_size, 'gamma': args.gamma, 'learning_rate': args.learning_rate, 'device': device}
+        
+    results_ppo = Resutls(title="Moving averaged episode reward", xlabel="episode", ylabel="running_score")
+    results_ppo.create_logs(labels=["episode", "running_score", "score"], init_values=[[], [], []])
+
+    results_vae = Resutls(title="Loss", xlabel="training_step", ylabel="loss")
+    results_vae.create_logs(labels=["training_step", "loss"], init_values=[[], []])
+
+    agent = Agent(model_config, results_vae)
+    env = Env()
+
+    running_score = 0
+    state = env.reset()
+    # 100000
+    for i_ep in range(3500):
+        score = 0
+        state = env.reset()
+
+        for t in range(1000):
+            action, a_logp = agent.select_action(state)
+            state_, reward, done, die = env.step(action * np.array([2., 1., 1.]) + np.array([-1., 0., 0.]))
+            if args.render:
+                env.render()
+            if agent.store((state, action, a_logp, reward, state_)):
+                print('updating')
+                agent.update()
+            score += reward
+            state = state_
+            if done or die:
+                break
+        running_score = running_score * 0.99 + score * 0.01
+
+        if i_ep % args.log_interval == 0:
+            results.update_logs(["episode", "running_score", "score"], [i_ep, running_score, score])
+            print('Ep {}\tLast score: {:.2f}\tMoving average score: {:.2f}'.format(i_ep, score, running_score))
+            agent.save_param()
+            results_ppo.save_logs('/home/mila/l/lea.cote-turcotte/LUSR/logs', str(1))
+            results_vae.save_logs('/home/mila/l/lea.cote-turcotte/LUSR/logs', str(2))
+        if running_score > env.reward_threshold:
+            results_ppo.save_logs('/home/mila/l/lea.cote-turcotte/LUSR/logs', str(1))
+            results_vae.save_logs('/home/mila/l/lea.cote-turcotte/LUSR/logs', str(2))
+            results_ppo.generate_plot('/home/mila/l/lea.cote-turcotte/LUSR/logs/1/args.json','/home/mila/l/lea.cote-turcotte/LUSR/figures')
+            results_vae.generate_plot('/home/mila/l/lea.cote-turcotte/LUSR/logs/2/args.json','/home/mila/l/lea.cote-turcotte/LUSR/figures')
+            print("Solved! Running reward is now {} and the last episode runs to {}!".format(running_score, score))
+            break
+    results_ppo.save_logs('/home/mila/l/lea.cote-turcotte/LUSR/logs', str(1))
+    results_vae.save_logs('/home/mila/l/lea.cote-turcotte/LUSR/logs', str(2))
+    results_ppo.generate_plot('/home/mila/l/lea.cote-turcotte/LUSR/logs/1/args.json', '/home/mila/l/lea.cote-turcotte/LUSR/figures')
+    results_vae.generate_plot('/home/mila/l/lea.cote-turcotte/LUSR/logs/2/args.json','/home/mila/l/lea.cote-turcotte/LUSR/figures')
--- LUSR/utils.py
+++ LUSR/utils.py
@@ -2,8 +2,14 @@ import torch
 from torch.utils.data.sampler import BatchSampler, SubsetRandomSampler
 from torch.utils.data import Dataset, DataLoader
 from skimage.util.shape import view_as_windows
+from torchvision import transforms
 import os
 import numpy as np
+import json
+import matplotlib.pyplot as plt
+import random
+import cv2
+from torchvision.utils import save_image
 
 
 def reparameterize(mu, logsigma):
@@ -75,3 +81,147 @@ def random_crop(imgs, output_size):
     # selects a random window for each batch element
     cropped_imgs = windows[np.arange(n), w1, h1]
     return cropped_imgs
+
+class RandomTransform():
+    def __init__(self, imgs):
+        self.imgs = imgs
+
+    def apply_random_transformation(self):
+        imgs = []
+        #n = self.imgs.shape[0]
+        for img in self.imgs:
+            p = random.uniform(0, 1)
+            if p > 0.75:
+                imgs.append(self.random_crop(img))
+            elif 0.25 < p < 0.75:
+                imgs.append(self.random_blur(img))
+            else:
+                imgs.append(self.random_jitter(img))
+        return torch.stack(imgs)
+
+    def apply_transformations(self):
+        n = self.imgs.shape[0]
+        m = int(n/4)
+        imgs_crop = self.random_crop(self.imgs[0:m, :, :, :])
+        imgs_blur = self.random_blur(self.imgs[m:2*m, :, :, :])
+        imgs_jitter = self.random_jitter(self.imgs[2*m:3*m, :, :, :])
+        imgs_norm = self.random_color_grass(self.imgs[3*m:4*m, :, :, :])
+        return torch.stack([imgs_crop, imgs_blur, imgs_jitter, imgs_norm])
+
+    def random_jitter(self, imgs):
+        imgs = transforms.ColorJitter(brightness=(0.5,1.5), contrast=(1), saturation=(0.5,1.5), hue=(-0.1,0.1))(imgs)
+        return imgs
+
+    def random_blur(self, imgs):
+        img = transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5))(imgs)
+        return imgs
+
+    def random_crop(self, imgs, output_size=64):
+        img = transforms.RandomCrop((output_size, output_size), padding=10)(imgs)
+        return imgs
+
+    def change_color_grass(self, img, color):
+        save_image(img, "/home/mila/l/lea.cote-turcotte/LUSR/figures/test0.png")
+        img = np.array(img.permute(2, 1, 0).cpu(), dtype="uint8")
+        save_image(torch.tensor(img, dtype=torch.float).permute(2, 1, 0).to('cuda'), "/home/mila/l/lea.cote-turcotte/LUSR/figures/test1.png")
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        mask_green = cv2.inRange(hsv, (36, 25, 25), (70, 255, 255))
+        imask_green = mask_green>0
+        green = np.zeros_like(img, np.uint8)
+        green[imask_green] = img[imask_green]
+        save_image(torch.tensor(green, dtype=torch.float).permute(2, 1, 0).to('cuda'), "/home/mila/l/lea.cote-turcotte/LUSR/figures/test5.png")
+        green = np.ones_like(img, np.uint8)
+        green[imask_green] = color
+        save_image(torch.tensor(green, dtype=torch.float).permute(2, 1, 0).to('cuda'), "/home/mila/l/lea.cote-turcotte/LUSR/figures/test2.png")
+        img = img*green
+        save_image(torch.tensor(img, dtype=torch.float).permute(2, 1, 0).to('cuda'), "/home/mila/l/lea.cote-turcotte/LUSR/figures/test3.png")
+        return torch.tensor(img, dtype=torch.float).permute(2, 1, 0).to('cuda')
+
+    def change_color_grass_test(self, img, color):
+        img = np.array(img.permute(2, 1, 0).cpu(), dtype="uint8")
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        mask_green = cv2.inRange(hsv, (36, 25, 25), (70, 255, 255))
+        imask_green = mask_green>0
+        green = np.zeros_like(img, np.uint8)
+        green[imask_green] = img[imask_green]
+        return torch.tensor(green, dtype=torch.float).permute(2, 1, 0).to('cuda')
+
+    def random_color_grass(self, images):
+        imgs = []
+        for img in images:
+            p = random.uniform(0, 1)
+            if p > 0.75:
+                imgs.append(self.change_color_grass(img, [0.5,1,0.5]))
+                save_image(self.change_color_grass(img, [0.5,1,0.5]), "/home/mila/l/lea.cote-turcotte/LUSR/figures/test4.png")
+            elif 0.5 < p < 0.75:
+                imgs.append(self.change_color_grass(img, [0.3,1,1]))
+                save_image(self.change_color_grass_test(img, [0.5,1,0.5]), "/home/mila/l/lea.cote-turcotte/LUSR/figures/test6.png")
+            elif 0.25 < p < 0.5:
+                imgs.append(self.change_color_grass(img, [0.3,0.1,1]))
+            else:
+                imgs.append(img)
+        return torch.stack(imgs)
+
+class Resutls():
+    def __init__(self, title, xlabel=None, ylabel=None):
+        self.logs = {}
+        self.title = title
+        self.xlabel = xlabel
+        self.ylabel = ylabel
+
+    def create_logs(self, labels, init_values):
+        for label, value in zip(labels, init_values):
+            assert type(value) == list
+            self.logs[label] = value
+
+    def update_logs(self, labels, values):
+        for label, value in zip(labels, values):
+            self.logs[label].append(value)
+
+    def save_logs(self, log_dir, exp_id):
+        log_dir = os.path.join(log_dir, exp_id)
+        os.makedirs(log_dir, exist_ok=True)
+        with open(os.path.join(log_dir, "args.json"), "w") as f:
+            json.dump(self.logs, f, indent=2)
+
+    def generate_plot(self, logdir, save_path):
+        """ Generate plot according to log 
+        :param logdir: path to log directories
+        :param save_path: Path to save the fig
+        """
+        data = {}
+        json_path = os.path.join(logdir, 'args.json')
+        assert os.path.exists(os.path.join(logdir, 'args.json')), f"No json file in {logdir}"
+        with open(json_path, 'r') as f:
+            data[self.title] = json.load(f)
+
+        fig, ax = plt.subplots()
+        ax.plot(data[self.title][self.xlabel], data[self.title][self.ylabel], label=self.title)
+        ax.set_xlabel(self.xlabel)
+        ax.set_ylabel(self.ylabel.replace('_', ' '))
+        fig.suptitle(self.title)
+        fig.savefig(os.path.join(save_path, f'{self.ylabel}.png'))
+
+
+    def generate_plots(self, list_of_dirs, legend_names, save_path):
+        """ Generate plots according to log 
+        :param list_of_dirs: List of paths to log directories
+        :param legend_names: List of legend names
+        :param save_path: Path to save the figs
+        """
+        assert len(list_of_dirs) == len(legend_names), "Names and log directories must have same length"
+        data = {}
+        for logdir, name in zip(list_of_dirs, legend_names):
+            json_path = os.path.join(logdir, 'args.json')
+            assert os.path.exists(os.path.join(logdir, 'args.json')), f"No json file in {logdir}"
+            with open(json_path, 'r') as f:
+                data[name] = json.load(f)
+    
+        for ylabel, xlabel in zip(self.ylabel, self.xlabel):
+            fig, ax = plt.subplots()
+            for name in data:
+                ax.plot(data[name][ylabel], label=name)
+            ax.legend()
+            ax.set_xlabel(xlabel)
+            ax.set_ylabel(ylabel.replace('_', ' '))
+            fig.savefig(os.path.join(save_path, f'{ylabel}.png'))
